a88dfc7ad680d8a8c978f2fa976d43d8dedf8591
buentellopc <buentellopc@gmail.com>
2023-04-11 20:38:01 -0600

    Add Character and List functionality to retrieve data from API

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,45 @@
+import { useEffect, useState } from "react";
 import Character from "./Character";
 
 function List() {
-  return;
-  <div>
-    <h2>Characters</h2>;
-    <Character />
-    <Character />
-    <Character />
-  </div>;
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(characters);
+  console.log(characters.length);
+  console.log("helloooo");
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetch("https://rickandmortyapi.com/api/character");
+      const { results } = await data.json();
+      setCharacters(results);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, [characters.length]);
+
+  console.log(characters.length);
+
+  return (
+    <div>
+      <h2>Characters</h2>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        characters.map((character) => {
+          return (
+            <Character
+              key={character.id}
+              name={character.name}
+              origin={character.origin}
+              image={character.image}
+            />
+          );
+        })
+      )}
+    </div>
+  );
 }
 
 export default List;
