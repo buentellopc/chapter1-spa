94eebfe3a6e652e3a771404b8b634784dfb5cb65
buentellopc <buentellopc@gmail.com>
2023-04-16 13:50:55 -0600

    Add: first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('should render Rick and Morty title', () => {
+test("should render Rick and Morty title", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Rick and Morty");
   expect(linkElement).toBeInTheDocument();
 });
